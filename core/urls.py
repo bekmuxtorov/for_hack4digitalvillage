e3d8846a4851700c7cb7f94c9577224dd8373cc7
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-11-24 17:22:30 +0500

    ➕ Added admin site title

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_title = "Terminal Group"
+admin.site.site_header = "Terminal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("base.urls"))
